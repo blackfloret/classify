ff60fc6dc18b43b825627d786b8bab92510203ee
Derrick Black <36499349+blackfloret@users.noreply.github.com>
2022-05-21 18:18:32 -0700

    Created Activities and Fragments according to skeleton

---
+++ app/src/main/java/com/example/classify/PetCareActivity.kt
@@ -0,0 +1,11 @@
+package com.example.classify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PetCareActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pet_care)
+    }
+}
