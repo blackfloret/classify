dddf090e17346b17495c0cfafa461b9e3c867e3b
phamd7 <71897082+phamd7@users.noreply.github.com>
2022-05-23 15:23:15 -0700

    Added Canvas API link

--- app/src/main/java/com/example/classify/ScheduleActivity.kt
+++ app/src/main/java/com/example/classify/ScheduleActivity.kt
@@ -3,6 +3,8 @@ package com.example.classify
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Canvas API Reference: https://canvas.instructure.com/doc/api/index.html
+
 class ScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
