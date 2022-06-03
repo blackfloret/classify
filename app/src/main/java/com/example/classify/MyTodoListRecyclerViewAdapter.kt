13d92dc3d7526dac1f1a97525ed7cb00ee5283de
Peter Beck <dipham99@gmail.com>
2022-06-02 18:58:24 -0700

    Adding 'BalanceListener' interface to Recycler view holder. Will implement in Main and Schedule

--- app/src/main/java/com/example/classify/MyTodoListRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/classify/MyTodoListRecyclerViewAdapter.kt
@@ -7,12 +7,19 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.classify.databinding.TodoItemBinding
 import com.example.classify.placeholder.PlaceholderContent.PlaceholderItem
+import java.time.Duration
+import java.time.LocalDate
+import java.time.Period
 import java.time.format.DateTimeFormatter
 
 // Define a listener interface for this fragment
 interface AdapterListener {
     fun onTodoClick(position: Int)
     fun onTodoRemove(priority: Int)
+}
+
+interface BalanceListener {
+    fun onAddBalance(value: Int)
 }
 
 /**
@@ -24,6 +31,7 @@ class MyTodoListRecyclerViewAdapter(
 ) : RecyclerView.Adapter<MyTodoListRecyclerViewAdapter.ViewHolder>() {
 
     var listener: AdapterListener? = null
+    var balanceListener: BalanceListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(
@@ -44,12 +52,30 @@ class MyTodoListRecyclerViewAdapter(
             listener?.onTodoRemove(item.priority)
         }
 
+        // Calculate the value of the TodoItem
+        val today: LocalDate = LocalDate.now()
+        var period = Period.between(item.date, today)
+
+        if (period.days <= 1) {
+            holder.moneyView.text = "+1"
+            balanceListener?.onAddBalance(1)
+        } else if (period.days <= 3) {
+            holder.moneyView.text = "+5"
+            balanceListener?.onAddBalance(5)
+        } else {
+            holder.moneyView.text = "+10"
+            balanceListener?.onAddBalance(10)
+        }
+
+        // Priority
         holder.nameView.text = item.priority.toString() + ". " + item.name
 
+        // Date
         var formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy")
         var formattedDate = item.date.format(formatter)
         holder.dateView.text = formattedDate
 
+        // Time
         var minStr = ""
         if (item.minute < 10) {
             minStr = "0" + item.minute.toString()
@@ -69,6 +95,7 @@ class MyTodoListRecyclerViewAdapter(
         }
         holder.timeView.text = timeStr
 
+        // Comment
         holder.commentView.text = item.comment
     }
 
@@ -82,6 +109,7 @@ class MyTodoListRecyclerViewAdapter(
 
     inner class ViewHolder(binding: TodoItemBinding) : RecyclerView.ViewHolder(binding.root) {
         val xButton: ImageView = binding.xButton
+        val moneyView: TextView = binding.coinText
         val nameView: TextView = binding.todoName
         val dateView: TextView = binding.todoDate
         val timeView: TextView = binding.todoTime
