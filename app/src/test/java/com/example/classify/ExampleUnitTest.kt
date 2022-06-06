e34e0997f18505cba2fc62af2b5a95d436ae85f1
Peter Beck <dipham99@gmail.com>
2022-06-05 22:29:23 -0700

    In progress with unit tests

--- app/src/test/java/com/example/classify/ExampleUnitTest.kt
+++ app/src/test/java/com/example/classify/ExampleUnitTest.kt
@@ -1,9 +1,14 @@
 package com.example.classify
 
 import android.util.Log
+import com.nhaarman.mockito_kotlin.given
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.spy
+import com.nhaarman.mockito_kotlin.whenever
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 import java.time.LocalDate
 import java.util.*
 
@@ -13,103 +18,113 @@ import java.util.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private lateinit var scheduleActivity: ScheduleActivity
+
+    @Before
+    fun setup() {
+        scheduleActivity = mock()
+        val list = scheduleActivity.TODO_LIST
+
+
+        whenever(scheduleActivity.onTodoEntered(localDate, hour, minute, name, comment, priority))
+            .thenAnswer {
+                list.add(ToDoData(localDate, hour, minute, name, comment, priority))
+            }
+    }
+
     @Test
     fun todoAdded() {
-        ScheduleActivity().TODO_LIST = arrayListOf<ToDoData>()
-
-        ScheduleActivity().onTodoEntered(LocalDate.of(2010, 12, 30), 1, 1,"", "", 1)
-        ScheduleActivity().onTodoEntered(LocalDate.of(2020, 12, 30), 2, 2,"", "", 2)
-        ScheduleActivity().onTodoEntered(LocalDate.of(2030, 12, 30), 3, 3,"", "", 3)
+        scheduleActivity.onTodoEntered(LocalDate.of(2010, 12, 30), 1, 1,"", "", 1)
+        scheduleActivity.onTodoEntered(LocalDate.of(2020, 12, 30), 2, 2,"", "", 2)
+        scheduleActivity.onTodoEntered(LocalDate.of(2030, 12, 30), 3, 3,"", "", 3)
 
         assertEquals(ToDoData(LocalDate.of(2010, 12, 30), 1, 1,"", "", 1),
-            ScheduleActivity().TODO_LIST[0]
+            scheduleActivity.TODO_LIST[0]
         )
 
         assertEquals(ToDoData(LocalDate.of(2020, 12, 30), 2, 2,"", "", 2),
-            ScheduleActivity().TODO_LIST[1]
+            scheduleActivity.TODO_LIST[1]
         )
 
         assertEquals(ToDoData(LocalDate.of(2030, 12, 30), 3, 3,"", "", 3),
-            ScheduleActivity().TODO_LIST[2]
+            scheduleActivity.TODO_LIST[2]
         )
     }
 
     @Test
     fun todoRemoved() {
-        ScheduleActivity().TODO_LIST = arrayListOf<ToDoData>()
+        scheduleActivity.TODO_LIST[0] = ToDoData(LocalDate.of(2000, 12, 30), 23, 59,"", "", 1)
+        scheduleActivity.TODO_LIST[1] = ToDoData(LocalDate.of(2010, 12, 30), 23, 59,"", "", 2)
+        scheduleActivity.TODO_LIST[2] = ToDoData(LocalDate.of(2020, 12, 30), 23, 59,"", "", 3)
 
-        ScheduleActivity().TODO_LIST[0] = ToDoData(LocalDate.of(2000, 12, 30), 23, 59,"", "", 1)
-        ScheduleActivity().TODO_LIST[1] = ToDoData(LocalDate.of(2010, 12, 30), 23, 59,"", "", 2)
-        ScheduleActivity().TODO_LIST[2] = ToDoData(LocalDate.of(2020, 12, 30), 23, 59,"", "", 3)
-
-        ScheduleActivity().onTodoRemove(1, 2)
+        scheduleActivity.onTodoRemove(1, 2)
         assertEquals(ToDoData(LocalDate.of(2000, 12, 30), 23, 59,"", "", 1),
-            ScheduleActivity().TODO_LIST[0]
+            scheduleActivity.TODO_LIST[0]
         )
 
         assertEquals(ToDoData(LocalDate.of(2030, 12, 30), 3, 3,"", "", 2),
-            ScheduleActivity().TODO_LIST[1]
+            scheduleActivity.TODO_LIST[1]
         )
     }
 
     @Test
     fun todoPriorityChangedOnInsert() {
-        ScheduleActivity().TODO_LIST[0] = ToDoData(LocalDate.of(2000, 12, 30), 23, 59,"", "", 1)
-        ScheduleActivity().TODO_LIST[1] = ToDoData(LocalDate.of(2010, 12, 30), 23, 59,"", "", 2)
-        ScheduleActivity().TODO_LIST[2] = ToDoData(LocalDate.of(2020, 12, 30), 23, 59,"", "", 3)
-        ScheduleActivity().TODO_LIST[3] = ToDoData(LocalDate.of(2040, 12, 30), 4, 4,"", "", 4)
-        ScheduleActivity().onTodoEntered(LocalDate.of(2050, 12, 30), 5, 5,"", "", 2)
+        scheduleActivity.TODO_LIST[0] = ToDoData(LocalDate.of(2000, 12, 30), 23, 59,"", "", 1)
+        scheduleActivity.TODO_LIST[1] = ToDoData(LocalDate.of(2010, 12, 30), 23, 59,"", "", 2)
+        scheduleActivity.TODO_LIST[2] = ToDoData(LocalDate.of(2020, 12, 30), 23, 59,"", "", 3)
+        scheduleActivity.TODO_LIST[3] = ToDoData(LocalDate.of(2040, 12, 30), 4, 4,"", "", 4)
+        scheduleActivity.onTodoEntered(LocalDate.of(2050, 12, 30), 5, 5,"", "", 2)
 
 
         assertEquals(ToDoData(LocalDate.of(2000, 12, 30), 23, 59,"", "", 1),
-            ScheduleActivity().TODO_LIST[0]
+            scheduleActivity.TODO_LIST[0]
         )
 
         assertEquals(ToDoData(LocalDate.of(2050, 12, 30), 5, 5,"", "", 2),
-            ScheduleActivity().TODO_LIST[1]
+            scheduleActivity.TODO_LIST[1]
         )
 
         assertEquals(ToDoData(LocalDate.of(2010, 12, 30), 23, 59,"", "", 3),
-            ScheduleActivity().TODO_LIST[0]
+            scheduleActivity.TODO_LIST[0]
         )
 
         assertEquals(ToDoData(LocalDate.of(2020, 12, 30), 23, 59,"", "", 4),
-            ScheduleActivity().TODO_LIST[0]
+            scheduleActivity.TODO_LIST[0]
         )
 
         assertEquals(ToDoData(LocalDate.of(2040, 12, 30), 4, 4,"", "", 5),
-            ScheduleActivity().TODO_LIST[0]
+            scheduleActivity.TODO_LIST[0]
         )
 
-        assertEquals(1, ScheduleActivity().TODO_LIST[0].priority)
-        assertEquals(2, ScheduleActivity().TODO_LIST[1].priority)
-        assertEquals(3, ScheduleActivity().TODO_LIST[2].priority)
-        assertEquals(4, ScheduleActivity().TODO_LIST[3].priority)
-        assertEquals(5, ScheduleActivity().TODO_LIST[4].priority)
+        assertEquals(1, scheduleActivity.TODO_LIST[0].priority)
+        assertEquals(2, scheduleActivity.TODO_LIST[1].priority)
+        assertEquals(3, scheduleActivity.TODO_LIST[2].priority)
+        assertEquals(4, scheduleActivity.TODO_LIST[3].priority)
+        assertEquals(5, scheduleActivity.TODO_LIST[4].priority)
     }
 
     @Test
     fun TODO_LIST_size_increase(){
-        ScheduleActivity().TODO_LIST = arrayListOf<ToDoData>()
+        scheduleActivity.TODO_LIST = arrayListOf<ToDoData>()
 
-        ScheduleActivity().onTodoEntered(LocalDate.of(2010, 12, 30), 1, 1,"", "", 1)
-        ScheduleActivity().onTodoEntered(LocalDate.of(2020, 12, 30), 2, 2,"", "", 2)
-        ScheduleActivity().onTodoEntered(LocalDate.of(2030, 12, 30), 3, 3,"", "", 3)
-        ScheduleActivity().onTodoEntered(LocalDate.of(2040, 12, 30), 4, 4,"", "", 4)
+        scheduleActivity.onTodoEntered(LocalDate.of(2010, 12, 30), 1, 1,"", "", 1)
+        scheduleActivity.onTodoEntered(LocalDate.of(2020, 12, 30), 2, 2,"", "", 2)
+        scheduleActivity.onTodoEntered(LocalDate.of(2030, 12, 30), 3, 3,"", "", 3)
+        scheduleActivity.onTodoEntered(LocalDate.of(2040, 12, 30), 4, 4,"", "", 4)
 
-        assertEquals(4, ScheduleActivity().TODO_LIST.size)
+        assertEquals(4, scheduleActivity.TODO_LIST.size)
     }
 
     @Test
     fun TODO_LIST_size_decrease(){
-        ScheduleActivity().TODO_LIST = arrayListOf<ToDoData>()
-        ScheduleActivity().TODO_LIST[0] = ToDoData(LocalDate.of(2000, 12, 30), 23, 59,"", "", 1)
-        ScheduleActivity().TODO_LIST[1] = ToDoData(LocalDate.of(2010, 12, 30), 23, 59,"", "", 2)
-        ScheduleActivity().TODO_LIST[2] = ToDoData(LocalDate.of(2020, 12, 30), 23, 59,"", "", 3)
+        scheduleActivity.TODO_LIST = arrayListOf<ToDoData>()
+        scheduleActivity.TODO_LIST[0] = ToDoData(LocalDate.of(2000, 12, 30), 23, 59,"", "", 1)
+        scheduleActivity.TODO_LIST[1] = ToDoData(LocalDate.of(2010, 12, 30), 23, 59,"", "", 2)
+        scheduleActivity.TODO_LIST[2] = ToDoData(LocalDate.of(2020, 12, 30), 23, 59,"", "", 3)
 
-        ScheduleActivity().onTodoRemove(1, 3)
+        scheduleActivity.onTodoRemove(1, 3)
 
-        assertEquals(2, ScheduleActivity().TODO_LIST.size)
+        assertEquals(2, scheduleActivity.TODO_LIST.size)
     }
 
 }
